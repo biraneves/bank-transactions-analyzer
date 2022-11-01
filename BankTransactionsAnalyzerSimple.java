a03b9e3356f4d3a90e39f1ca1614c55a0df6ec1f
Ubirajara Neves <ubirajara.neves@usp.br>
2022-11-01 19:01:21 -0300

    Calculating only for January

--- BankTransactionsAnalyzerSimple.java
+++ BankTransactionsAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionsAnalyzerSimple {
@@ -10,14 +13,19 @@ public class BankTransactionsAnalyzerSimple {
     public static void main(String[] args) throws IOException {
         final Path path = Paths.get(RESOURCES + args[0]);
         final List<String> lines = Files.readAllLines(path);
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         double total = 0d;
 
         for (final String line: lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
 
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total for all transactions in January is " + total);
     }
 }
